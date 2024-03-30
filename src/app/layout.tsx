599d5ae49a26f074ddfaf5c6bd456a01ced54caa
Abbasa5251 <abbasa5251@hotmail.com>
2024-03-31 01:31:01 +0530

    feat: added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
@@ -44,6 +45,7 @@ export default function RootLayout({
 				<body className={`${inter.className} bg-dark-2`}>
 					{children}
 					<Toaster />
+					<SpeedInsights />
 				</body>
 			</ClerkProvider>
 		</html>
