a8a52f4f48eecd6a72db9996b6e7524c2035ce4b
Abbasa5251 <abbasa5251@hotmail.com>
2024-04-10 02:22:08 +0530

    feat: add theme toggle on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import MobileNav from "./MobileNav";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { ThemeToggle } from "./ThemeToggle";
 
 const Navbar = () => {
 	return (
-		<nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+		<nav className="flex-between fixed z-50 w-full bg-white dark:bg-dark-1 px-6 py-4 lg:px-10">
 			<Link href={"/"} className="flex items-center gap-1">
 				<Image
 					src={"/icons/logo.svg"}
@@ -14,12 +15,13 @@ const Navbar = () => {
 					width={32}
 					height={32}
 				/>
-				<p className="text-[26px] font-extrabold text-white max-sm:hidden">
+				<p className="text-[26px] font-extrabold text-main-1 dark:text-white max-sm:hidden">
 					ADev Zoom
 				</p>
 			</Link>
 
 			<div className="flex-between gap-5">
+				<ThemeToggle />
 				<SignedIn>
 					<UserButton afterSignOutUrl="/sign-in" />
 				</SignedIn>
